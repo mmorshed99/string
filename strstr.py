f1e5de15cceef74782bfac5422611786b07e0629
M Manzur Morshed <mmorshed@asu.edu>
2017-10-23 23:17:08 -0400

    Update strstr.py

--- strstr.py
+++ strstr.py
@@ -1,33 +1,41 @@
-def find_strstr(A,B):
-    A_list = list(A)
-    B_list = list(B)
-    if len(A_list)==0 or len(B_list)==0:
-        return -1
-    curr_master = A_list[0]
-    curr_slave = B_list[0]
-    master_id = 0
-    slave_length = len(B_list)-1
-    curr_length = 0
-    while(True):
-        if curr_master == curr_slave:
-            if curr_length == slave_length:
-                return master_id-slave_length
-            if master_id < len(A_list)-1:
-                curr_master = A_list[master_id+1]
-                master_id += 1
-                curr_length += 1
-                curr_slave = B_list[curr_length]
+#Implement strStr().
+#
+#strstr - locate a substring ( needle ) in a string ( haystack ). 
+#
+#
+class Solution:
+    # @param A : string
+    # @param B : string
+    # @return an integer
+    def strStr(self, A, B):
+     def find_strstr(A,B):
+        A_list = list(A)
+        B_list = list(B)
+        if len(A_list)==0 or len(B_list)==0:
+            return -1
+        curr_master = A_list[0]
+        curr_slave = B_list[0]
+        master_id = 0
+        slave_length = len(B_list)-1
+        curr_length = 0
+        while(True):
+            if curr_master == curr_slave:
+                if curr_length == slave_length:
+                    return master_id-slave_length
+                if master_id < len(A_list)-1:
+                    curr_master = A_list[master_id+1]
+                    master_id += 1
+                    curr_length += 1
+                    curr_slave = B_list[curr_length]
+                else:
+                    return -1
             else:
-                return -1
-        else:
-            curr_length = 0
-            curr_slave = B_list[0]
-            if master_id < len(A_list)-1:
-                curr_master = A_list[master_id+1]
-                master_id += 1
-            else:
-                return -1
-           
-A = 'GeeksForGeeks'
-B = 'For'
-print(find_strstr(A,B))
+                master_id = master_id - curr_length
+                curr_length = 0
+                curr_slave = B_list[0]
+                if master_id < len(A_list)-1:
+                    curr_master = A_list[master_id+1]
+                    master_id += 1
+                else:
+                    return -1
+     return find_strstr(A,B) 
