ad64f643ec5cfd9151ab819d229bdbc31a8b2b7b
M Manzur Morshed <mmorshed@asu.edu>
2018-10-04 15:49:34 -0400

    Update length_of_last_word.py

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -16,3 +16,24 @@ class Solution:
                 found_char = 1
                 mylen += 1
         return mylen
+### another day
+class Solution:
+    # @param A : string
+    # @return an integer
+    def lengthOfLastWord(self, A):
+        count = 0
+        if len(A) == 0:
+            return 0
+        i = len(A)-1
+        while(i >= 0):
+            if A[i] == " ":
+                i -= 1
+                continue
+            else:
+                while(A[i] != " "):
+                    count += 1
+                    i -= 1
+                    if i < 0:
+                        break
+                break
+        return count
